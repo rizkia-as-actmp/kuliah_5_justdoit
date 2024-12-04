7ca09b25fe43783f1f1c7d962a28cb7e9b05d148
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-12-04 11:29:44 +0000

    -

--- lib/src/features/authentication/data/auth_repository.dart
+++ lib/src/features/authentication/data/auth_repository.dart
@@ -1,1 +1,11 @@
-class HttpAuthRepository {}
+class HttpAuthRepository {
+
+
+
+  }
+
+// if you're making multiple requests to the same server, you can keep open a persistent connection by using a Client rather than making one-off requests.
+//
+// If you do this, make sure to close the client when you're done:
+//
+// The interface for HTTP clients that take care of maintaining persistent connections across multiple requests to the same server.
