5e7cd096b295a54b0c72b720cb843fc4a9c47cf1
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-12-14 20:05:45 +0900

    -

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:justdoit/src/features/authentication/data/secure_storage_repository.dart';
+import 'package:justdoit/src/features/authentication/data/shared_preferences_repository.dart';
+import 'package:justdoit/src/features/authentication/presentation/otp/otp_screen.dart';
 import 'package:justdoit/src/features/authentication/presentation/register/register_user_screen.dart';
 import 'package:justdoit/src/common_widgets/loading_screen.dart';
 import 'package:justdoit/src/common_widgets/splash_screen.dart';
@@ -8,7 +12,22 @@ import 'package:justdoit/src/features/authentication/presentation/register/login
 import 'package:justdoit/src/features/authentication/presentation/register/register_complete_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  //Untuk membuat widget dapat membaca provider, kita perlu membungkus seluruh aplikasi dengan widget "ProviderScope". Di sinilah state dari provider kita disimpan.
+  runApp(
+    const ProviderScope(child: MyApp()),
+  );
+}
+
+class _RiverpodEagerInitialization extends ConsumerWidget {
+  const _RiverpodEagerInitialization({required this.child});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // dart tidak memungkian bagi riverpod prodiver untuk melakukan Eagerly initialization, alternatifnya kita harus force read provider agar ter init di awal
+    ref.watch(otpIdSharedPrefRepositoryProvider);
+    return child;
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -17,22 +36,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return _RiverpodEagerInitialization(
+        child: MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: DefinedTheme.background),
+        colorScheme: ColorScheme.fromSeed(seedColor: DefinedTheme.greyish),
         useMaterial3: true,
       ),
-      initialRoute: '/activities',
+      initialRoute: '/verify-otp',
       routes: {
         '/': (context) => const RegisterUserScreen(),
         '/login': (context) => const LoginUserScreen(),
+        '/verify-otp': (context) => const InputOtpScreen(),
         '/registration-complete': (context) => const RegisterCompleteScreen(),
         '/loading': (context) => const LoadingScreen(),
         '/splash': (context) => const SplashScreen(),
         '/activities': (context) => const ActivitiesScreen()
       },
-    );
+    ));
   }
 }
