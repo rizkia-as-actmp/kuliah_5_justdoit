6cab31035b02dbe10d8abad8f2e5f68502ae3f28
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-12-17 06:18:42 +0900

    feat: add registerUser method in AuthService to handle user registration

--- lib/src/features/authentication/application/auth_service.dart
+++ lib/src/features/authentication/application/auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:justdoit/src/features/authentication/data/auth_repository.dart';
 import 'package:justdoit/src/features/authentication/data/shared_preferences_repository.dart';
 import 'package:justdoit/src/exceptions/custom_exception.dart';
+import 'package:justdoit/src/features/authentication/domain/user.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'auth_service.g.dart';
 
@@ -9,18 +10,33 @@ class AuthService extends _$AuthService {
   @override
   void build() {}
 
+  Future<User> registerUser(
+      {required String name,
+      required String email,
+      required String password}) async {
+    try {
+      final user = await ref
+          .read(authRepositoryProvider)
+          .registerUser(name, email, password);
+
+      return user;
+    } catch (e) {
+      if (e is CustomException) rethrow;
+      throw CustomException(id: "ded49291", details: e);
+    }
+  }
+
   Future<bool> authWithPassword({
     required String email,
     required String password,
   }) async {
     try {
       final auth = await ref
-          .watch(authRepositoryProvider)
+          .read(authRepositoryProvider)
           .authWithPassword(email, password);
       await ref
-          .watch(otpIdSharedPrefRepositoryProvider.notifier)
+          .read(otpIdSharedPrefRepositoryProvider.notifier)
           .writeData(auth.otpId!);
-      //print(await ref .watch(otpIdSharedPrefRepositoryProvider.notifier) .readData());
     } catch (e) {
       if (e is CustomException) rethrow;
       throw CustomException(id: "c432149f", details: e);
@@ -34,10 +50,8 @@ class AuthService extends _$AuthService {
   }) async {
     try {
       final otpId =
-          ref.watch(otpIdSharedPrefRepositoryProvider.notifier).readData();
-      print(otpId);
-      final auth =
-          await ref.watch(authRepositoryProvider).verifyOtp(otpId, otp);
+          ref.read(otpIdSharedPrefRepositoryProvider.notifier).readData();
+      final auth = await ref.read(authRepositoryProvider).verifyOtp(otpId, otp);
       //await ref .watch(otpIdSharedPrefRepositoryProvider.notifier) .writeData(auth.otpId!);
       //print(await ref .watch(otpIdSharedPrefRepositoryProvider.notifier) .readData());
     } catch (e) {
