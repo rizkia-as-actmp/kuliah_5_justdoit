b0608bbfc9ec9e6c080c81304ca13491951d5312
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-12-17 06:11:42 +0900

    feat: add registerUser method to HttpAuthRepository for user registration

--- lib/src/interfaces/http_auth_repository.dart
+++ lib/src/interfaces/http_auth_repository.dart
@@ -1,11 +1,12 @@
 import 'package:justdoit/src/features/authentication/domain/auth.dart';
+import 'package:justdoit/src/features/authentication/domain/user.dart';
 import 'package:justdoit/src/interfaces/http_repository.dart';
 
 abstract interface class HttpAuthRepository implements HttpRepository {
+  Future<User> registerUser(String name, String email, String password);
   Future<PbAuth> authWithPassword(String email, String password);
   Future<PbAuth> verifyOtp(String otpId, String otp);
   //Future<void> logoutUser();
-  //Future<void> registerUser(String email, String password, Map<String, dynamic> additionalData);
   //Future<void> resetPassword(String email);
   //Future<void> changePassword(String oldPassword, String newPassword);
   //Future<void> refreshToken(String refreshToken);
