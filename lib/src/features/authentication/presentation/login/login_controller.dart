0f794e87471c600a38d8abf26d728fdbd5d37227
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-12-21 10:39:42 +0900

    -

--- lib/src/features/authentication/presentation/login/login_controller.dart
+++ lib/src/features/authentication/presentation/login/login_controller.dart
@@ -2,27 +2,6 @@ import 'package:justdoit/src/features/authentication/application/auth_service.da
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 part 'login_controller.g.dart';
 
-// @riverpod
-// class LoginController extends _$LoginController {
-//   @override
-//   FutureOr<String> build() {
-//     return 'tes';
-//   }
-//
-//   Future<void> submit({
-//     required String email,
-//     required String password,
-//   }) async {
-//     // Set the state to loading
-//     state = const AsyncValue.loading();
-//     state = await AsyncValue.guard(() async {
-//       await Future.delayed(const Duration(seconds: 3));
-//       //throw Exception();
-//       return "tes";
-//     });
-//   }
-// }
-
 @riverpod
 class LoginController extends _$LoginController {
   @override
