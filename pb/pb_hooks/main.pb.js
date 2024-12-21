bba97987382efd04d69e533587cfa9710eea764c
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-12-21 10:29:16 +0900

    -

--- pb/pb_hooks/main.pb.js
+++ pb/pb_hooks/main.pb.js
@@ -1,5 +1,9 @@
 /// <reference path="../pb_data/types.d.ts" />
 routerAdd("GET", "/hello", (e) => {
+  // const record = $app.findRecordById('users', 'qvp20oq6xkv053t');
+  // console.log(JSON.stringify(
+  //   record.tokenKey()
+  // ))
 
   return e.json(200, { success: "always" })
 })
