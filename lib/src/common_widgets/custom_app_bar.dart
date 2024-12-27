298794941d96e75c8a9c38402e17aa596078fbb4
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-12-28 08:20:56 +0900

    refactor(common_widgets): enhance CustomAppBar with customizable widget rows

--- lib/src/common_widgets/custom_app_bar.dart
+++ lib/src/common_widgets/custom_app_bar.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:justdoit/src/common_widgets/custom_sized_box.dart';
-import 'package:justdoit/src/common_widgets/type.dart';
-import 'package:justdoit/src/constants/colors.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({super.key});
+  CustomAppBar({
+    super.key,
+    this.rightRowWidgets = const <Widget>[],
+    this.middleRowWidgets = const <Widget>[],
+    this.leftRowWidgets = const <Widget>[],
+    this.automaticallyImplyLeading = false,
+  });
+
+  List<Widget> rightRowWidgets;
+  List<Widget> middleRowWidgets;
+  List<Widget> leftRowWidgets;
+  bool automaticallyImplyLeading;
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
@@ -13,50 +22,38 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       automaticallyImplyLeading:
-          false, // NOTE: untuk menghilangkan back button pada leading
-      //title: HeadingThree(data: "Activity"),
+          automaticallyImplyLeading, // NOTE: untuk menghilangkan back button pada leading
+      // app bar actions mendorong widgets dari sebelah kanan ke kiri
       actions: [
         mediumHSizedBox,
-        HeadingThree(data: "Activity"),
-        bigHSizedBox,
         Expanded(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Flexible(
-                child: SizedBox(
-                  width: 640,
-                  height: 40,
-                  child: SearchBar(
-                    elevation: WidgetStateProperty.all(0),
-                    backgroundColor:
-                        WidgetStateProperty.all(DefinedTheme.primary50),
-                    trailing: const [
-                      Icon(
-                        Icons.search,
-                        color: DefinedTheme.primary,
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        mediumHSizedBox,
-        IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.more_vert,
-              color: DefinedTheme.onBackground,
-            )),
-        IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.account_circle_outlined,
-              color: DefinedTheme.onBackground,
-            )),
-        const SizedBox(width: 16),
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            // entah mengapa jika container diberikan warna, size dari widget akan berubah dan lebih mudah untuk diatur dalam kasus ini
+            //Container(
+            //  color: Colors.cyan,
+            //  child: Expanded(
+            //    child: Row(
+            //      mainAxisAlignment: MainAxisAlignment.start,
+            //      children: leftRowWidgets,
+            //    ),
+            //  ),
+            //),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: leftRowWidgets,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: middleRowWidgets,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: rightRowWidgets,
+            )
+          ],
+        )),
         mediumHSizedBox,
       ],
     );
