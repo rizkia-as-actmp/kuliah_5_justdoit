e17975e84968436ef4606fab10770e2ecf8b3109
Rizkia Adhy Syahputra <rizkia.as.pac@gmail.com>
2024-12-30 22:22:34 +0900

    -

--- lib/src/features/authentication/application/auth_providers.g.dart
+++ lib/src/features/authentication/application/auth_providers.g.dart
@@ -6,11 +6,11 @@ part of 'auth_providers.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$authHash() => r'70a060abc71af40bbb80df1d3ca821fb1566d296';
+String _$authHash() => r'1a3452586b4c598218d1fe06029e3ea160ac72d5';
 
 /// See also [Auth].
 @ProviderFor(Auth)
-final authProvider = AutoDisposeAsyncNotifierProvider<Auth, PbAuth?>.internal(
+final authProvider = AutoDisposeAsyncNotifierProvider<Auth, String?>.internal(
   Auth.new,
   name: r'authProvider',
   debugGetCreateSourceHash:
@@ -19,6 +19,6 @@ final authProvider = AutoDisposeAsyncNotifierProvider<Auth, PbAuth?>.internal(
   allTransitiveDependencies: null,
 );
 
-typedef _$Auth = AutoDisposeAsyncNotifier<PbAuth?>;
+typedef _$Auth = AutoDisposeAsyncNotifier<String?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
